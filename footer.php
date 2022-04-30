c367693e28e009e083b16618fdd853b223cd35c6
[KRiki] <[kpower2487@gmail.com]>
2022-04-30 19:17:34 +0900

    second commit

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-
 <?php wp_footer(); ?>
 <div class="contact">
     <div class="contact-con">
@@ -9,25 +8,28 @@
                 <a href="tel:000-1234-5678">000-0000-000</a>
             </div>
             <div class="contact-way">
-                <a href="<?php echo get_page_link( 7 );?>">お問い合わせフォーム</a>
+                <a href="<?php echo get_page_link(7); ?>">お問い合わせフォーム</a>
             </div>
 
         </div>
     </div>
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/Chat.png" alt="">
 </div>
+test
 
 <div class="about">
     <div class="about-con">
         <h2>株式会社<br>Guest</h2>
         <p>愛知県半田市のWEB制作会社。<br>東海地方を中心に対応しておりますが、オンラインでもご利用可能なので全国どこでもお受けしております。<br>その他にもテニススクールの運営やHR事業を行っております。
-    <br>お気軽にご連絡ください。</p>
+            <br>お気軽にご連絡ください。
+        </p>
     </div>
 
 </div>
 
-    <footer>
-        <p>Copyright (c) Guest. All rights reserved.</p>
-    </footer>
+<footer>
+    <p>Copyright (c) Guest. All rights reserved.</p>
+</footer>
 </body>
+
 </html>
